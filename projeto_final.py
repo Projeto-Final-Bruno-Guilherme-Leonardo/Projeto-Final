e3f787c84b973ee617e4fc7635be4eb9904fbdd4
guilhermearanha <guilhermessaranha@gmail.com>
2020-06-01 15:03:58 -0300

    atualizacao

--- projeto_final.py
+++ projeto_final.py
@@ -7,8 +7,8 @@ import pygame
 import sys
 import random
 
-surf_altura = 500
-surf_largura = 700
+surf_altura = 600
+surf_largura = int(surf_altura * 1.5)
 
 pygame.init() # inicia o pygame
 
@@ -16,17 +16,22 @@ surf = pygame.display.set_mode([surf_largura, surf_altura]) # crição da superf
 
 coisa_imagens = []
 
-v = 10
+game = 0    #o jogo começa desligado, no menu
+
+v = 10  #velocida dos obstaculos
+
+def proporcao(a, b):
+    return (int(a * surf_altura / 100), int(b * surf_altura / 100))
 
 try:
     imagem_jetpack = pygame.image.load('imagens/jetpack.png').convert_alpha()
-    imagem_jetpack = pygame.transform.scale(imagem_jetpack, (100, 100))
+    imagem_jetpack = pygame.transform.scale(imagem_jetpack, proporcao(10, 10))
     
     coisa_imagens.append(pygame.image.load('imagens/pedra.png').convert_alpha())
-    coisa_imagens[-1] = pygame.transform.scale(coisa_imagens[-1], (50, 50))
+    coisa_imagens[-1] = pygame.transform.scale(coisa_imagens[-1], proporcao(50, 50))
     
     coisa_imagens.append(pygame.image.load('imagens/laser.png').convert_alpha())
-    coisa_imagens[-1] = pygame.transform.scale(coisa_imagens[-1], (100, 100))
+    coisa_imagens[-1] = pygame.transform.scale(coisa_imagens[-1], proporcao(50, 50))
     
 except pygame.error:
     print('Erro ao tentar ler uma imagem')
@@ -34,29 +39,36 @@ except pygame.error:
     
 
 class Jetpack(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+    def __init__(self, grupo):
+        super().__init__(grupo)
         
         self.image = imagem_jetpack
         self.rect = self.image.get_rect()
-        self.rect.centerx = 100
-        self.rect.centery = 100
-        self.speed = 0
-        self.a = 0  #aceleracao
+        self.rect.left = 100
+        self.rect.bottom = surf_altura
+        self.speed = 0  #velocidade vertical da jetpack
+        self.a = 1  #aceleracao
         
     def update(self):
-        self.speed += self.a
-        self.rect.y += 1 * self.speed
+        self.speed += self.a * 2
+        self.rect.y += self.speed
         if self.rect.top + (1 * self.speed) < 0:
             self.rect.top = 0
             self.speed = 0
         if self.rect.bottom + (1 * self.speed) > surf_altura:
             self.rect.bottom = surf_altura
             self.speed = 0
+    
+    def reset(self):
+        self.rect.left = 100
+        self.rect.bottom = surf_altura
+        self.speed = 0  #velocidade vertical da jetpack
+        self.a = 1  #aceleracao
+        
             
 class Coisa(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+    def __init__(self, grupo):
+        super().__init__(grupo)
         
         self.image = coisa_imagens[random.randint(0, len(coisa_imagens) -1)]
         self.rect = self.image.get_rect()
@@ -69,15 +81,16 @@ class Coisa(pygame.sprite.Sprite):
             self.kill()
         
         
-    
 
-
-jetpack = Jetpack()
 
 sprites = pygame.sprite.Group()
-sprites.add(jetpack)
+coisas = pygame.sprite.Group()
+
+jetpack = Jetpack(sprites)
+
 
 def main():
+    global game
     
     clock = pygame.time.Clock()
     FPS = 30
@@ -87,39 +100,57 @@ def main():
     # Game Loop
     while True:
         clock.tick(FPS)
-        timer += 1
         
-        if timer == 100:
-            timer = 0
-            coisa = Coisa()
-            sprites.add(coisa)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit() # terminado a aplicação pygame
-                sys.exit() # sai pela rotima do sistema
+                pygame.quit()   #fecha o pygame
+                sys.exit()      #fecha o sistema
                 
             if event.type == pygame.KEYDOWN:
-            # Dependendo da tecla, altera a velocidade.
                 if event.key == pygame.K_SPACE:
                     jetpack.a = -1
-        # Verifica se soltou alguma tecla.
+                if event.key == pygame.K_RETURN and game == 0:
+                    game = 1
+                    
             if event.type == pygame.KEYUP:
-            # Dependendo da tecla, altera a velocidade.
                 if event.key == pygame.K_SPACE:
                     jetpack.a = 1
-                    
-        sprites.update()
         
-        surf.fill((255, 255, 255)) # preenche a tela
-        #cores variam de 0 a 255 > 0 = preto
-        sprites.draw(surf)
+        if game:    #se o jogo estiver ligado:
+            
+            timer += 1
+            if timer == 100:
+                timer = 0
+                coisa = Coisa(sprites)
+                coisas.add(coisa)
+                print('criando coisa')
+                        
+            sprites.update()
+            
+            surf.fill((255, 255, 255)) # preenche a tela
+            #cores variam de 0 a 255 > 0 = preto
+            sprites.draw(surf)
 
         pygame.display.flip() # faz a atualizacao da tela
+
+        if pygame.sprite.spritecollide(jetpack, coisas, 0):
+            game = 0
+            for i in coisas:
+                i.kill()
+            jetpack.reset()
+
+        print('a')
+        print(sprites)
 
 
 
 main()
+
+print('aaaaa')
+
+
+
+
 
 
 
